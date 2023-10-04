cefcd8a750a393017faee5d2fa7c8b62e415d714
edsonmendz <japaact@gmail.com>
2023-10-04 20:24:41 -0300

    fim

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { initializeApp } from "firebase/app";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'instagram-clone-Angular';
+
+  ngOnInit(): void {
+
+    const firebaseConfig = {
+      apiKey: "AIzaSyBage6ZGXYOrQtHj4VAfiA5q0NX1rhN97A",
+      authDomain: "jta-instagram-clone-483ee.firebaseapp.com",
+      projectId: "jta-instagram-clone-483ee",
+      storageBucket: "jta-instagram-clone-483ee.appspot.com",
+      messagingSenderId: "786654825951",
+      appId: "1:786654825951:web:d606b8e8ff49183d6d486a"
+    };
+
+    initializeApp(firebaseConfig)
+  }
 }
--- src/app/autenticacao.service.ts
+++ src/app/autenticacao.service.ts
@@ -1,7 +1,30 @@
+import { initializeApp } from "firebase/app";
 import { Usuario } from "./acesso/usuario.model";
+import * as firebase from "firebase/auth";
+import { getDatabase, connectDatabaseEmulator, ref } from "firebase/database";
 
 export class Autenticacao {
     public cadastrarUsuario(usuario:Usuario): void {
-        console.log('chegamos até aqui ', usuario)
+
+        const auth = firebase.getAuth()
+        const database = getDatabase()
+        
+        firebase.createUserWithEmailAndPassword( auth, usuario.email, usuario.senha)
+            .then((resposta:any) => {
+                // TODO: Replace the following with your app's Firebase project configuration
+                // See: https://firebase.google.com/docs/web/learn-more#config-object
+                const firebaseConfig = {
+                    // ...
+                    // The value of `databaseURL` depends on the location of the database
+                    databaseURL: "https://DATABASE_NAME.firebaseio.com",
+                };
+                
+                // Initialize Firebase
+                const app = initializeApp(firebaseConfig);
+                
+                
+                // Initialize Realtime Database and get a reference to the service
+                const database = getDatabase(app);
+                            })
     }
 }
